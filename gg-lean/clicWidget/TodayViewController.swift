f1247965cdde4ac7c00b37244a28299ca40df071
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2017-07-14 10:23:30 -0300

    Widget load tasks not working

--- gg-lean/clicWidget/TodayViewController.swift
+++ gg-lean/clicWidget/TodayViewController.swift
@@ -13,10 +13,12 @@ class TodayViewController: UIViewController, NCWidgetProviding {
 
     @IBOutlet weak var tableView: UITableView!
     let manager = DataBaseManager.shared
+    var showMore : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//        loadTasks()
         extensionContext?.widgetLargestAvailableDisplayMode = .expanded
     }
     
@@ -27,8 +29,8 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     func loadTasks(){
         manager.getTasks { (tasks) in
-            Cache.shared().tasks = tasks
             
+            Cache.shared().tasks = tasks
         }
     }
     
@@ -46,20 +48,21 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-        
-        manager.getTasks { (tasks) in
-            Cache.shared().tasks = tasks
+        Cache.shared().updateTasks(completionHandler: { (tasks) in
             print("tasks no widget:", Cache.shared().tasks)
+            self.tableView.reloadData()
             completionHandler(NCUpdateResult.newData)
-        }
-        
+        })
         
     }
     
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
         let expanded = activeDisplayMode == .expanded
+        
+        self.showMore = expanded ? true : false
         preferredContentSize = expanded ? CGSize(width: maxSize.width,
-                                                 height: CGFloat(tableView.numberOfRows(inSection: 1))*tableView.rowHeight + 200.0): maxSize
+                                                 height: 250): maxSize
+        tableView.reloadData()
     }
     
 }
@@ -68,15 +71,26 @@ extension TodayViewController : UITableViewDelegate, UITableViewDataSource{
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 5
+        if self.showMore == true{
+            return 3
+        }
+        
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "widgetCell", for: indexPath)
+        print("Widget trying to create and load cell")
+        print( Cache.shared().tasks,  Cache.shared().tasks.count)
         
-        cell.detailTextLabel?.text = "00:00"
-        cell.textLabel?.text = "Teste de Label"
+        let task = Cache.shared().tasks[indexPath.row]
+        print("Widget: \(task.name)")
+        //cell.detailTextLabel?.text = task.getTimeString()
+        //cell.textLabel?.text = task.name
+        
+        cell.detailTextLabel?.text = "ksdhfb"
+        cell.textLabel?.text = "???r"
         
         return cell
     }
